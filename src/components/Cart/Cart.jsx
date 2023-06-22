7771d9130267489e5ba754cc8bb48d46d18ef6e9
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-06-22 12:42:34 +0530

    Payment integration added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,8 +4,27 @@ import { BsCartX } from "react-icons/bs";
 import "./Cart.scss";
 import { useContext } from 'react';
 import { Context } from './../../utils/context';
+import { loadStripe } from "@stripe/stripe-js";
+import { makePaymentRequest } from '../../utils/api';
+
 const Cart = ({ setShowCart }) => {
-    const { cartItems, cartSubtotal } = useContext(Context)
+    const { cartItems, cartSubtotal } = useContext(Context);
+
+    const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+    const handlePayment = async () => {
+
+        try {
+            const stripe = await stripePromise;
+            const res = await makePaymentRequest.post("/api/orders", {
+                products: cartItems,
+            });
+            await stripe.redirectToCheckout({
+                sessionId: res.data.stripeSession.id
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
         <div className="cart-panel">
             <div className="opac-layer"></div>
@@ -31,7 +50,7 @@ const Cart = ({ setShowCart }) => {
                             <span className="text total">&#8377;{cartSubtotal}</span>
                         </div>
                         <div className="button">
-                            <button className='checkout-cta'>Checkout</button>
+                            <button className='checkout-cta' onClick={handlePayment}>Checkout</button>
                         </div>
                     </div>
                 </>}
